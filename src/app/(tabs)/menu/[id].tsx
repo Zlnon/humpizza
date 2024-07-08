86a6d7da49f3f5c71ac5136f0a9de562ea86b49d
Zlnon <mtarekk666@gmail.com>
2024-07-08 21:12:22 +0300

    itemDetails Screen

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,14 +1,60 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Image, Pressable } from "react-native";
+import React, { useState } from "react";
 import { Stack, useLocalSearchParams } from "expo-router";
+import products from "../../../../assets/data/products";
+import { defaultPizzaImage } from "../../../components/ProductListItem";
+import Button from "../../../components/Button";
+
+const sizes = ["S", "M", "L", "XL"];
 
 const productItemDetails = () => {
   const { id } = useLocalSearchParams();
 
+  const [selectedSize, setSelectedSize] = useState("M");
+
+  const product = products.find((p) => p.id.toString() === id);
+
+  const addToCart = () => {
+    console.warn("Adding to cart", selectedSize);
+  };
+  if (!product) {
+    return <Text> product not found</Text>;
+  }
+
   return (
-    <View className="bg-white h-full">
-      <Stack.Screen options={{ title: "Details:" + id }} />
-      <Text className="text-2xl">productItemDetails{id}</Text>
+    <View className="bg-white h-full flex-1 p-2">
+      <Stack.Screen options={{ title: product?.name }} />
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        resizeMode="contain"
+        className="w-[100%] aspect-square"
+      />
+      <View className="mt-2">
+        <Text className="text-xl font-semibold">Select Size</Text>
+
+        <View className="flex justify-around flex-row my-3 ">
+          {sizes.map((size) => (
+            <Pressable
+              key={size}
+              onPress={() => {
+                setSelectedSize(size);
+              }}
+              className={`bg-gray-300 w-12 aspect-square rounded-full justify-center items-center 
+                ${selectedSize === size ? "bg-gray-200" : "bg-white"}`}
+            >
+              <Text
+                className={`text-xl font-semibold ${
+                  selectedSize === size ? "text-black" : "text-gray-300"
+                }`}
+              >
+                {size}
+              </Text>
+            </Pressable>
+          ))}
+        </View>
+      </View>
+      <Text className="text-2xl font-bold mt-auto">${product.price}</Text>
+      <Button onPress={addToCart} text="Add to cart" />
     </View>
   );
 };
