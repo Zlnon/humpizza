fdeaf82cd0e18578ec86a541eb0af0e69af1683a
Zlnon <mtarekk666@gmail.com>
2024-07-15 16:54:44 +0300

    auth Screen (ui2)

--- src/app/(auth)/sign-in.tsx
+++ src/app/(auth)/sign-in.tsx
@@ -1,23 +1,30 @@
 import { View, Text, TextInput } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import Button from "../../components/Button";
 import { Link, Stack } from "expo-router";
 
 const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <View className="bg-gray-50 h-full justify-center p-5">
       <Stack.Screen options={{ title: "Sign in" }} />
+
       <Text className="text-gray-400">Email</Text>
       <TextInput
+        value={email}
         className=" w-full bg-white border-2 border-gray-400  text-gray-400 rounded-lg p-2 mt-3"
         placeholder="name@email.com"
         keyboardType="email-address"
+        onChangeText={setEmail}
       />
       <Text className="text-gray-400 mt-4">Password</Text>
       <TextInput
+        value={password}
         className=" w-full bg-white border-2 border-gray-400  text-gray-400 rounded-lg p-2 mt-3"
         placeholder=""
         secureTextEntry
+        onChangeText={setPassword}
       />
       <View className="mt-5  w-full">
         <Button text=" Sign in" />
@@ -26,7 +33,6 @@ const SignIn = () => {
           href={"/sign-up"}
           className=" text-base  text-light-tint mt-4 self-center"
         >
-          {" "}
           Create an Account
         </Link>
       </View>
--- src/app/(auth)/sign-up.tsx
+++ src/app/(auth)/sign-up.tsx
@@ -1,9 +1,11 @@
 import { View, Text, TextInput } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import Button from "../../components/Button";
 import { Link, Stack } from "expo-router";
 
 const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <View className="bg-gray-50 h-full justify-center p-5">
       <Stack.Screen options={{ title: "Sign Up" }} />
@@ -12,12 +14,16 @@ const SignUp = () => {
         className=" w-full bg-white border-2 border-gray-400  text-gray-400 rounded-lg p-2 mt-3"
         placeholder="name@email.com"
         keyboardType="email-address"
+        value={email}
+        onChangeText={setEmail}
       />
       <Text className="text-gray-400 mt-4">Password</Text>
       <TextInput
         className=" w-full bg-white border-2 border-gray-400  text-gray-400 rounded-lg p-2 mt-3"
         placeholder=""
         secureTextEntry
+        value={password}
+        onChangeText={setPassword}
       />
       <View className="mt-5  w-full">
         <Button text=" Create Account " />
